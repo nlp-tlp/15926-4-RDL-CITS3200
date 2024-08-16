1345071d838080408e1585a65c5cf6ba3b4af7e7
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-08-16 18:18:38 +0800

    Fix linting issues example

--- src/server/app/config.py
+++ src/server/app/config.py
@@ -7,19 +7,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Shared configuration
 class Config:
-		SQLALCHEMY_TRACK_MODIFICATIONS = False
-		SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "default-key-for-testing")
-		# Set local secret key with
-		# BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
-		# WINDOWS CMD: set FLASK_SECRET_KEY=your_secret_key_here
-		# with dotenv, you can instead create a .env file and set a local secret key in the same style as Windows
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "default-key-for-testing")
+    # Set local secret key with
+    # BASH: echo 'export FLASK_SECRET_KEY="your_secret_key_here"' >> ~/.bashrc && source ~/.bashrc
+    # WINDOWS CMD: set FLASK_SECRET_KEY=your_secret_key_here
+    # with dotenv, you can instead create a .env file and set a local secret key in the same style as Windows
 
 
 class DeploymentConfig(Config):
-    #SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL') or 'sqlite:///../instance/database.db'
+    # SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL') or 'sqlite:///../instance/database.db'
     DEBUG = False
 
 
 class TestConfig(Config):
-    #SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:" 
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     DEBUG = True
