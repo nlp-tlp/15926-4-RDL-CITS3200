8b1fd7c15bca6a5987cda89608124acd98a44496
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-08-17 15:42:17 +0800

    Update installation script and documentation
    - Update containers to use node-20-slim.
    - Update setup script to fix husky pre-commit hooks.
    - Update documentation with how to setup development builds.

--- README.md
+++ README.md
@@ -3,9 +3,24 @@
 ## Visualisation of the ISO 15926-4 Reference Data Library
 
 ## Overview
-
 This project aims to develop an interactive visualisation for an equipment reference data library as described in the ISO 15926-4 Standard. The goal is to create a web-based interface that allows users to interrogate the data and visualise the hierarchical relations.
 
 ## Purpose
-
 The current browser, https://data.15926.org/rdl/, is frustrating to use due to the lack of hierarchical visualisation. This project aims to enhance the user experience and improve data accessibility.
+
+
+## Setup (Development)
+This project uses a mix of pre-commit checks and github actions in the workflow, ensuring clean code. To properly setup the project follow these steps.
+
+### Clone the repositry
+  `git clone https://github.com/nlp-tlp/15926-4-RDL-CITS3200.git`
+
+### Install dependencies
+  `./setup.sh` (If the script fails, please use `sudo`)
+
+### Build and run Docker containers
+`cd src`
+
+`docker compose up --build`
+
+- Note: Any changes to main files (dependencies, settings, etc.) require the containers to be rebuilt.
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Setting up the project..."
+echo -e "\nSetting up the project..."
 
 # Check if nvm is installed
 if ! command -v nvm &> /dev/null
 then
-    echo "nvm not found. Installing nvm..."
+    echo -e "\n\nnvm not found. Installing nvm..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
     # Load nvm after installation
@@ -14,41 +14,46 @@ then
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 else
     # Load nvm if it's already installed
-    echo "nvm found. Loading nvm..."
+    echo -e "\n\nnvm found. Loading nvm..."
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 
 # Use Node.js version 20
-echo "Switching to Node.js version 20..."
+echo -e "\n\nSwitching to Node.js version 20..."
 nvm install 20
 nvm use 20
 
 # Check if npm is installed
 if ! command -v npm &> /dev/null
 then
-    echo "npm is not installed. Please install Node.js properly."
+    echo -e "\n\nnpm is not installed. Please install Node.js properly."
     exit 1
 else
-    echo "npm found. Proceeding with installation..."
+    echo -e "\n\nnpm found. Proceeding with installation..."
 fi
 
 # Install Node.js dependencies in the main directory
-echo "Installing Node.js dependencies in the main directory..."
+echo -e "\n\nInstalling Node.js dependencies in the main directory..."
 npm install
 
 # Set up Husky
-echo "Setting up Husky..."
-npx husky install
+echo -e "\n\nSetting up Husky..."
+npx husky-init
+
+# Add pre-commit hook
+echo -e "\n\nAdding pre-commit hook to Husky..."
+sed -i '/npm test/d' .husky/pre-commit
+npx husky add .husky/pre-commit "cd src/client && npm run lint:fix && npm run format && npm run type-check && cd ../server && black ."
 
 # Install Python dependencies
-echo "Installing Python dependencies..."
+echo -e "\n\nInstalling Python dependencies..."
 pip install black
 
 # Navigate to the client directory and install Node.js dependencies
-echo "Installing Node.js dependencies in src/client..."
+echo -e "\n\nInstalling Node.js dependencies in src/client..."
 cd src/client || exit
 npm install
 
-echo "Setup complete."
+echo -e "\n\nSetup complete."
