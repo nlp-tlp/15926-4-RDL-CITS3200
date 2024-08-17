56b6971c4ac0a6c81b65885b8be6832e820450a4
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-08-17 14:46:57 +0800

    Update workflow and installation
    - Update eslint rules.
    - Remove pre-commit in favour of Husky.
    - Setup husky pre-commit checks.
    - Redid github workflows to seperate backend and frontend. Added
      Prettier and type-check to workflow as well.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-# Install pre-commit hooks
-echo "Installing pre-commit hooks..."
-pip install pre-commit
-pre-commit install
-echo "Setup complete. Pre-commit hooks are installed."
+echo "Setting up the project..."
 
-# Other stuff....
+# Check if nvm is installed
+if ! command -v nvm &> /dev/null
+then
+    echo "nvm not found. Installing nvm..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+    # Load nvm after installation
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+else
+    # Load nvm if it's already installed
+    echo "nvm found. Loading nvm..."
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+fi
+
+# Use Node.js version 22
+echo "Switching to Node.js version 22..."
+nvm install 22
+nvm use 22
+
+# Install Python dependencies
+echo "Installing Python dependencies..."
+pip install black
+
+# Navigate to the client directory and install Node.js dependencies
+echo "Installing Node.js dependencies..."
+cd src/client || exit
+npm install
+
+echo "Setup complete."
--- src/client/.eslintrc.cjs
+++ src/client/.eslintrc.cjs
@@ -6,12 +6,51 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-strongly-recommended',
-    'plugin:prettier/recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  plugins: [
+    'simple-import-sort', 
+    'import'
+  ],
+  rules: {
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'warn',
+    'vue/block-order': ['warn', {
+      'order': [[ 'script', 'template' ], 'style']
+    }],
+    'vue/order-in-components': ['warn', {
+      'order': [
+        'el',
+        'name',
+        'key',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'emits',
+        'setup',
+        'asyncData',
+        'data',
+        'computed',
+        'watch',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }]
   }
 }
