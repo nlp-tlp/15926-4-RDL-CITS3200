1d569f767e329ab326e288816fc64b81ac4e3964
paulm <119311801+paulmmaingi@users.noreply.github.com>
2024-08-24 00:44:05 +0800

    Added a Navigation bar with the routes to home, graph and documentation

---
+++ src/client/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import IconDocumentation from './icons/IconDocumentation.vue'
+import NavBarItem from './NavBarItem.vue'
+</script>
+
+<template>
+  <nav class="navbar">
+    <NavBarItem to="/" label="Home" />
+    <NavBarItem to="/graph" label="Graph" />
+    <NavBarItem to="/documentation" label="Documentation">
+      <template #icon>
+        <IconDocumentation class="navbar-icon" />
+      </template>
+    </NavBarItem>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  font-size: 16px;
+  /* Centres items in navbar horizontally */
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: var(--color-nav-background);
+  /* To ensure navbar is always on top */
+  z-index: 1000;
+  padding: 1rem 2rem;
+  box-shadow: 0 1px 4px var(--color-nav-background);
+}
+
+.navbar-icon {
+  vertical-align: middle;
+  margin-right: 0.4rem;
+  margin-bottom: 0.2rem;
+}
+</style>
---
+++ src/client/src/components/NavBarItem.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+interface NavBarItemProps {
+  to: string
+  label: string
+}
+
+const props = defineProps<NavBarItemProps>()
+</script>
+
+<template>
+  <RouterLink :to="props.to" class="navbar-item">
+    <slot name="icon"></slot>
+    {{ props.label }}
+  </RouterLink>
+</template>
+
+<style scoped>
+.navbar-item {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 2px solid var(--color-border);
+  color: var(--color-nav-text);
+}
+
+.navbar-item:first-of-type {
+  border: 0;
+}
+
+.navbar-item.router-link-exact-active {
+  color: var(--color-text);
+}
+
+.navbar-item.router-link-exact-active:hover {
+  background-color: transparent;
+}
+</style>
