a355d8bb0e485f5f060bf199f598907b8663c2a4
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-08-25 00:27:58 +0800

    Added CLI interface for database management
    - Implement CLI using 'typer' package.
    - Add history file to keep track of multiple databases. (Allow for
      future rollback)
    - Update SPARQL API URL to now give correct output.
    - Fix response conversion.

---
+++ src/server/cli/cli.py
@@ -0,0 +1,42 @@
+import typer
+import os
+import json
+from datetime import datetime
+
+from history import history_file_create
+from db import update_db
+
+# Global variables
+DB_VERSION = "24/8/24"
+VERSION_HISTORY_FILE = 1
+HISTORY_FILE = "history.json"
+
+
+def main(
+    db_update: bool = False,
+    fix: bool = False
+    ):
+    """
+    CLI tool to interact with the database.
+
+    Args:
+    - db_update (bool): To update database straight away.
+    - fix (bool): To scan database and fix any errors.
+    """
+    # Print MOTD
+    motd()
+
+    history_file_create()
+
+    print("UPDATING DB!")
+    update_db()
+
+
+
+def motd():
+    typer.echo("Welcome to Iso15926Vis CLI for Backend!")
+    typer.echo(f"Database is at version '{DB_VERSION}'.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/server/cli/history.py
@@ -0,0 +1,54 @@
+import json
+import os
+from datetime import datetime
+
+HISTORY_FILE = "history.json"
+VERSION_HISTORY_FILE = 1
+
+def history_file_create():
+    # Path to the history file
+    history_path = os.path.join(os.path.dirname(__file__), HISTORY_FILE)
+
+    # Initialize the history data structure
+    history_data = {
+        "version": VERSION_HISTORY_FILE,
+        "databases": [],
+        "current_db": ''
+    }
+
+    # Load existing history if the file exists
+    if not os.path.exists(history_path):
+        with open(history_path, "w") as f:
+            json.dump(history_data, f, indent=4)
+
+    print(f"History file created: '{history_path}'")
+
+
+def history_file_add_db(new_db_filename):
+    # Path to the history file
+    history_path = os.path.join(os.path.dirname(__file__), HISTORY_FILE)
+
+    # Initialize the history data structure
+    history_data = {
+        "version": VERSION_HISTORY_FILE,
+        "databases": [],
+        "current_db": ''
+    }
+
+    # Load existing history if the file exists
+    if os.path.exists(history_path):
+        with open(history_path, "r") as f:
+            history_data = json.load(f)
+
+    # Update the history with the new database entry
+    history_data["version"] = VERSION_HISTORY_FILE
+    history_data["databases"].append({
+        "filename": new_db_filename,
+        "created_at": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    })
+
+    # Save the updated history back to the file
+    with open(history_path, "w") as f:
+        json.dump(history_data, f, indent=4)
+
+    print(f"Added new DB to history file: '{new_db_filename}'")
