172b232bf7d921df60ea24c186b45a4fc1a04445
sellsol <leowheidi@gmail.com>
2024-08-30 01:33:18 +0800

    Vitest initial install and config, no failing files

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -2,17 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
-// https://vitejs.dev/config/
+// const path = require('path') // vitest config extends vite config
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+      // '@/': path.resolve(__dirname, './src')
     }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true
   }
 })
