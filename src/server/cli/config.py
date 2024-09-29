0c5755ccaa6dbae5ae3fbcac373ddda67405c245
sellsol <leowheidi@gmail.com>
2024-09-29 16:35:53 +0800

    change db volume to be managed within docker

--- src/server/cli/config.py
+++ src/server/cli/config.py
@@ -2,9 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-HISTORY_FILE = os.path.join(basedir, "../../db/history.json")
+HISTORY_FILE = os.path.join(basedir, "../db/history.json")
 HISTORY_VERSION = 1
-DATABASE_STORAGE_DIR = os.path.join(basedir, "../../db/storage")
+DATABASE_STORAGE_DIR = os.path.join(basedir, "../db/storage")
 
 SOURCE_OF_TRUTH = "http://190.92.134.58:8890/sparql"
 BATCH_SIZE = 10000
