ca9fea3f01565cafcb65d558fd777a83c1b65947
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-09-30 19:31:37 +0800

    Remove old styling methods in comments

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -17,29 +17,3 @@ import NavBar from './components/NavBar.vue'
     </main>
   </div>
 </template>
-
-<!-- <style scoped>
-header {
-  line-height: 1.75;
-  max-height: 100vh;
-}
-
-.main-content {
-  padding-top: 4.5rem;
-  padding-left: 1rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
