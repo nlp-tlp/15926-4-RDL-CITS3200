12e8d0f5b80e2dc60a9e9cbd5d79c196a1db1dec
sellsol <leowheidi@gmail.com>
2024-10-03 22:21:56 +0800

    Tried to use fully nginx without traefik

--- src/server/cli/reload.py
+++ src/server/cli/reload.py
@@ -1,10 +1,11 @@
 import typer
 import requests
+from cli.config import SERVER_PORT
 
 
 def reload_graph():
     try:
-        response = requests.get("http://127.0.0.1:5000/ctrl/reload")
+        response = requests.get(f"http://127.0.0.1:{SERVER_PORT}/ctrl/reload")
         if response.status_code == 200:
             typer.echo("Graph reloaded successfully.")
         else:
